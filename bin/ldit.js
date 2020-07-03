75986e6baf30552e68d394d9db4996bfb85eb1dd
陈昭华 <jerry@Jerrys-MacBook-Pro.local>
2020-07-03 13:25:14 +0800

    feat:npm init

---
+++ bin/ldit.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+console.log("Let's do it together.");
+var cmd = process.argv[2];
+if(cmd){
+    switch(cmd){
+        case "name":
+            break;
+        case "site":
+            break;
+        case "email":
+            break;
+    }
+}
